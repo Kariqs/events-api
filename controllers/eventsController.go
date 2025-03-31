7d7a9513223e9c2529bbee88ca2d2d3e3f897f38
Kariqs <kariukibenard189@gmail.com>
2025-03-31 13:43:21 +0300

    Added some routes.

--- controllers/eventsController.go
+++ controllers/eventsController.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
 	"github.com/Kariqs/events-api/initializers"
 	"github.com/Kariqs/events-api/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func CreateEvent(ctx *gin.Context) {
@@ -22,4 +24,81 @@ func CreateEvent(ctx *gin.Context) {
 	}
 	//Return the created event
 	ctx.JSON(http.StatusCreated, gin.H{"event": event})
+}
+
+func GetEvents(ctx *gin.Context) {
+	var events []models.Event
+	result := initializers.DB.Find(&events)
+	if result.Error != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "unable to fetch events.", "status": http.StatusBadRequest})
+	}
+	ctx.JSON(http.StatusOK, events)
+}
+
+func GetEvent(ctx *gin.Context) {
+	//Get Id from the URL
+	eventId := ctx.Param("id")
+
+	//Find event with the ID
+	var event models.Event
+	result := initializers.DB.First(&event, eventId)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+			return
+		}
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Database error"})
+		return
+	}
+
+	//Return the event
+	ctx.JSON(http.StatusOK, event)
+}
+
+func UpdateEvent(ctx *gin.Context) {
+	//Get Id and data from the request
+	var updateData models.Event
+	eventId := ctx.Param("id")
+	ctx.ShouldBindJSON(&updateData)
+	//Find event with the ID
+	var event models.Event
+	result := initializers.DB.First(&event, eventId)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
+		return
+	}
+	//Update the event
+	initializers.DB.Model(&event).Updates(updateData)
+
+	//Return the event
+	ctx.JSON(http.StatusOK, event)
+}
+
+func DeleteEvent(ctx *gin.Context) {
+	//Get Id from URL
+	eventId := ctx.Param("id")
+	var event models.Event
+
+	//Check if event exists
+	result := initializers.DB.First(&event, eventId)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+			return
+		}
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Database error"})
+		return
+	}
+
+	//Delete event
+	deleteResult := initializers.DB.Delete(&event)
+	if deleteResult.Error != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unabale to delete event"})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Event has been deleted", "deletedEvent": deleteResult})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,10 @@ import (
 )
 
 func RegisterRoutes(server *gin.Engine) {
+	server.GET("/events", controllers.GetEvents)
 	server.POST("/events", controllers.CreateEvent)
+	server.GET("/events/:id", controllers.GetEvent)
+	server.PUT("/events/:id", controllers.UpdateEvent)
+	server.DELETE("/events/:id", controllers.DeleteEvent)
 	server.Run()
 }
